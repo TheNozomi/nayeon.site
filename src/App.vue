f8fb4a4d8bb79b2bbb4f811d9bfc4fbe45be8a60
Nozomi <13172293+TheNozomi@users.noreply.github.com>
2021-01-31 17:47:31 -0300

    ny

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Literalmente nada, por ahora -->
+</template>
+
+<style>
+body {
+  background-image: url('./assets/ny.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+</style>
